75f47abd727fa76b470b6de5eafed4c7ddcaf8ac
James Mills <james@jamesmills.co.uk>
2012-04-16 13:53:05 +0100

    initial commit

---
+++ fuel/app/logs/2012/04/16.php
@@ -0,0 +1,7 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+Warning - 2012-04-16 12:16:49 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+Warning - 2012-04-16 12:30:33 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+Warning - 2012-04-16 12:32:10 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+Warning - 2012-04-16 12:34:07 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+Warning - 2012-04-16 12:41:09 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
